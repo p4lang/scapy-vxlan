5459b954e66925b878870b4ca841f51a6fc9ebe8
Jochen Bartl <jochen.bartl@gmail.com>
2010-05-18 00:07:57 +0200

    Fixed size of reserved field in ICMPv6 ND Redirect Option header
    Field size was changed from 2 to 6 bytes, according to the RFC.
    http://tools.ietf.org/html/rfc2461#page-31

---
+++ scapy/crypto/__init__.py
@@ -0,0 +1,15 @@
+## This file is part of Scapy
+## See http://www.secdev.org/projects/scapy for more informations
+## Copyright (C) Arnaud Ebalard <arno@natisbad.org>
+## This program is published under a GPLv2 license
+
+
+
+try:
+    import Crypto
+except ImportError:
+    import logging
+    log_loading = logging.getLogger("scapy.loading")
+    log_loading.info("Can't import python Crypto lib. Disabled certificate manipulation tools")
+else:
+    from scapy.crypto.cert import *
